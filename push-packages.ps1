bec7352c2dc9810445c41dc0a0b06a9c973fdb3a
l0nley <hofvl0nley@gmail.com>
2015-05-22 10:24:09 +0300

    lets try

--- push-packages.ps1
+++ push-packages.ps1
@@ -8,4 +8,5 @@ Write-Host "Discovering files...."
 $files = Get-ChildItem -Path "" -Filter "$($pattern)" -Recurse
 foreach($file in $files) {
 	 Write-Host $file.FullName
+	 $env:CACHED_NUGET push "$($file.FullName)" 63d8f776-2dd7-4d06-bd64-3a6c9b31e7eb -s https://www.myget.org/F/l0nley
 }
